ab8de2bd769fac63b81abd3313603d69529572c2
chentian <chentian@kayak.com.cn>
2021-02-08 18:48:18 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -102,6 +102,7 @@ const vueConfig = {
 
   devServer: {
     // development server port 8000
+    disableHostCheck: true,
     port: 8000
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     // proxy: {
