184a0d646a9505b9969e47a6a4a142d66a9e1420
chentian <chentian.ct@qq.com>
2021-02-21 18:25:17 +0800

    途胜教育模块修改

--- src/views/training/tusheng/components/StudyCost.vue
+++ src/views/training/tusheng/components/StudyCost.vue
@@ -26,136 +26,28 @@ export default {
     return {
       tab6: [
         {
-          label: '批次',
-          field: 'name',
-          form: 'select',
-          rules: [{ max: 15, message: '批次限制输入15位' }]
-        },
-        {
-          label: '层次',
-          field: 'phone',
-          form: 'select',
-          rules: [{ max: 15, message: '层次限制输入15位' }]
-        },
-        {
-          label: '院校',
-          field: 'card',
-          form: 'input',
-          rules: [{ max: 20, message: '院校限制输入20位' }]
-        },
-        {
-          label: '姓名',
-          field: 'cardNo',
-          form: 'input',
-          rules: [{ max: 15, message: '姓名限制输入15位' }]
-        },
-        {
-          label: '专业',
-          field: 'mz',
-          form: 'input',
-          rules: [{ max: 15, message: '限制输入15位' }]
-        },
-        {
-          label: '身份证号',
-          field: 'meal',
-          form: 'input',
-          rules: [{ max: 18, message: '身份证号限制输入18位' }]
-        },
-        {
-          label: '录入时间',
-          field: 'birth',
-          form: 'date',
-          rules: []
-        },
-        {
-          label: '总学费',
-          field: 'location',
-          form: 'input',
-          rules: [{ validator: validatorMoney('总学费') }]
-        },
-        {
-          label: '实际学费',
-          field: 'xz',
-          form: 'input',
-          rules: [{ validator: validatorMoney('实际学费') }]
-        },
-        {
-          label: '欠款',
+          label: '欠费',
           field: 'zy',
           form: 'input',
-          rules: [{ validator: validatorMoney('欠款') }]
+          rules: [{ validator: validatorMoney('欠费') }]
         },
         {
-          label: '第一年交费',
+          label: '补贴费用',
           field: 'mm',
           form: 'input',
-          rules: [{ validator: validatorMoney('第一年交费') }]
+          rules: [{ validator: validatorMoney('补贴费用') }]
         },
         {
-          label: '第二年交费',
+          label: '补贴到账时间',
           field: 'school',
-          form: 'input',
-          rules: [{ validator: validatorMoney('第二年交费') }]
+          form: 'date',
+          rules: [{ validator: validatorMoney('补贴到账时间') }]
         },
         {
-          label: '第三年交费',
+          label: '备注',
           field: 'byTime',
           form: 'input',
-          rules: [{ validator: validatorMoney('第三年交费') }]
-        },
-        {
-          label: '第四年交费',
-          field: 'bookNo',
-          form: 'input',
-          rules: [{ validator: validatorMoney('第四年交费') }]
-        },
-        {
-          label: '第五年交费',
-          field: 'bookType',
-          form: 'input',
-          rules: [{ validator: validatorMoney('第五年交费') }]
-        },
-        {
-          label: '报考费',
-          field: 'address',
-          form: 'input',
-          rules: [{ validator: validatorMoney('报考费') }]
-        },
-        {
-          label: '入学考试辅导费',
-          field: 'work',
-          form: 'input',
-          rules: [{ validator: validatorMoney('入学考试辅导费') }]
-        },
-        {
-          label: '学位报考费',
-          field: 'email',
-          form: 'input',
-          rules: [{ validator: validatorMoney('学位报考费') }]
-        },
-        {
-          label: '学位辅导费',
-          field: 'aa',
-          form: 'input',
-          rules: [{ validator: validatorMoney('学位辅导费') }]
-        },
-        {
-          label: '网课费',
-          field: 'bb',
-          form: 'input',
-          rules: [{ validator: validatorMoney('网课费') }]
-        },
-        {
-          label: '论文费',
-          field: 'cc',
-          form: 'input',
-          rules: [{ validator: validatorMoney('论文费') }]
-        },
-        {
-          label: '图像采集费',
-          field: 'dd',
-          form: 'input',
-          rules: [{ validator: validatorMoney('图像采集费') }]
+          rules: []
         }
       ]
     }
